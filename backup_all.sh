be07531bd752f7c92b4a1380d7d7eba22274b180
Anon ODROID <anon@odroid>
2021-03-05 10:04:10 +0000

    Update backup scripts

--- backup_all.sh
+++ backup_all.sh
@@ -12,12 +12,12 @@ FIND="/usr/bin/find"
 CRONTAB="/usr/bin/crontab"
 #RMFIND="/bin/find"
 
-EXCLUDE_CONF="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/exclude.files.conf"
+EXCLUDE_CONF="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/all_system/exclude.files.conf"
 
 # Store todays date
 NOW=$(date +"%F"_"%H"-"%M")
 BKDIR="monthly"
-BACKUPDIR="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/$BKDIR"
+BACKUPDIR="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/all_system/$BKDIR"
 #Get the number of the day of the week
 DAY=$(date +%e)
 
@@ -33,7 +33,7 @@ TARDIR="./"
 BFILE="$NOW.tar.gz"
 
 # Store backup path
-LAST_MONTH="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/last_month"
+LAST_MONTH="/srv/dev-disk-by-label-DOCUMENTS/anon/odroid/backup/all_system/last_month"
 
 # make sure backup directory exists
 [ ! -d $BACKUPDIR ] && mkdir -p ${BACKUPDIR}
@@ -46,7 +46,7 @@ $LOGGER "$0: *** ${DOMAIN} ${BKDIR} Backup started @ $(date) ***"
 rm -rf ${SNAPSHOT_FILE}
 
 #If it's Sunday - we delete the initial metadata file and archives
-if [ $DAY = "5" ]; then
+if [ $DAY = "4" ]; then
  NUM="0"
  mkdir -p ${LAST_MONTH}
  rm -rf ${LAST_MONTH}/*
@@ -64,17 +64,14 @@ fi
 
 # Backup websever dirs
 #$TAR -zcvf ${BACKUP}/${BFILE} "${DIRS}"
-$TAR  --exclude-from=${EXCLUDE_CONF} --listed-incremental=${SNAPSHOT_FILE} -zcvf ${BACKUP}/${BFILE} -C ${CDDIR} "${TARDIR}"
+$TAR  --exclude-from=${EXCLUDE_CONF} --listed-incremental=${SNAPSHOT_FILE} -zcvf ${BACKUPDIR}/${BFILE} -C ${CDDIR} "${TARDIR}"
 
 $CRONTAB -l > $BACKUPDIR/crontab_$NOW
 
 #If it's Sunday, create an initial copy of the metadata
-if [ "$DAY" = "5" ]; then 
+if [ $DAY = "4" ]; then 
 	 cp ${SNAPSHOT_FILE} ${SNAPSHOT_FILE_0}
 fi
-
-
-
 
 # Log backup end time in /var/log/messages
 $LOGGER "$0: *** ${DOMAIN} ${BKDIR} Backup Ended @ $(date) ***"
